81af1b5da157d291c592bffccaa948ace3f2a3d3
胡嘉仕 <125730892@qq.com>
2019-02-18 15:46:48 +0800

    projict init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+
+import './assets/styles/reset.css'// 重置样式
+import './assets/styles/border.css'// 1px边框解决方案
 
 Vue.config.productionTip = false
+fastClick.attach(document.body) // 移动点击延迟
 
 /* eslint-disable no-new */
 new Vue({
