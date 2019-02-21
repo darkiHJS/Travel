dd66919c521efb6c00aed337ac801bff8e0bf74f
胡嘉仕 <125730892@qq.com>
2019-02-21 10:26:45 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input class="search-input" v-model="keyword" type="text" placeholder="输入城市名或拼音" />
+    </div>
+    <div class="search-content"
+      v-show="keyword"
+    >
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{ item.name }}
+        </li>
+        <li v-show="hasNoData" class="search-item border-bottom">
+          没有匹配项
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,6 +22,37 @@
 export default {
   name: 'CitySearch',
   props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        let result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
   }
 }
 </script>
@@ -26,5 +71,19 @@ export default {
       line-height .62rem
       text-align center
       border-radius .06rem
+      color #666
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
       color #666
 </style>
