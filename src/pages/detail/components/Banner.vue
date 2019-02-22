404f07893db4c4309cef57ebe4c2a1880742aaf3
胡嘉仕 <125730892@qq.com>
2019-02-22 14:50:25 +0800

    detail banner coding

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="banner">
-    <img src="" alt="">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1809/75/756c6ed69c72ec41a3.img.jpg_600x330_f3aec735.jpg" />
+    <div class="banner-info">
+      <div class="banner-title">南浔古镇(AAAAA景区)</div>
+      <div class="banner-number"><span class="iconfont">&#xe67c;</span>11</div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +14,41 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+      z-index -1
+    .banner-info
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      .banner-title
+        position relative
+        line-height .4rem
+        color #fff
+        left .2rem
+        bottom .2rem
+        font-size .36rem
+        text-shadow 0 1px 2px rgba(0,0,0,.7)
+      .banner-number
+        position absolute
+        right .2rem
+        bottom .2rem
+        width 1.2rem
+        height .4rem
+        background rgba(0,0,0,.5)
+        border-radius .2rem
+        font-size .24rem
+        color #fff
+        line-height .4rem
+        text-align center
+        .iconfont
+          font-size .3rem
 </style>
